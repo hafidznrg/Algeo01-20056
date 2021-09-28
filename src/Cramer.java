ef8b33c6a749e3d1ccc11bf9746dd4a003433db8
Fikri Khoiron Fadhila <79701028+fikrikhoironn@users.noreply.github.com>
2021-09-28 20:32:45 +0700

    Update src/Cramer.java
    Co-authored-by: Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>

--- src/Cramer.java
+++ src/Cramer.java
@@ -17,49 +17,45 @@ public class Cramer extends Utils {
         }
     }
 
-    public static void CramerRule(double[][] matrix) {
+    public static String[] cramerRule(double[][] matrix) {
+        // PREKONDISI: DETERMINAN MATRIKS != 0
+
         int row = matrix.length;
         int col = matrix[0].length;
-        double determinan = Cofactor.determinan(matrix);
+        String[] result = new String[col-1];
 
-        if (determinan == 0){
-            System.out.println("Determinan matrix ini = 0, silakan menggunakan cara lain");
+        //model Ax = b
+        //membuat matrix b dari matrix awal
+        double[][] b = new double[row][1];
+        for (int i = 0; i < row; i++){
+            b[i][0] = matrix[i][col-1];
         }
-        else{
-            //model Ax = b
-            //membuat matrix b dari matrix awal
-            double[][] b = new double[row][1];
-            for (int i = 0; i < row; i++){
-                b[i][0] = matrix[i][col-1];
-            }
-            //System.out.println("ini matriks b ");
-            //printMatrix(b);
-            //menghitung nilai x[1 ... col-1]
-            for (int k = 0; k < col - 1; k++) {
-                //membuat matrix a sementara untuk setiap colom
-                double[][] a = new double[row][col-1];
-                for (int i = 0; i < row; i++) {
-                    for (int j = 0; j <= col-2; j++){
-                        if (k == j){
-                            a[i][j] = b[i][0];
-                        }
-                        else {
-                            a[i][j] = matrix[i][j];
-                        }
+        //System.out.println("ini matriks b ");
+        //printMatrix(b);
+        //menghitung nilai x[1 ... col-1]
+        for (int k=0;k<(col-1);k++) {
+            //membuat matrix a sementara untuk setiap colom
+            double[][] a = new double[row][col-1];
+            for (int i=0;i<row;i++) {
+                for (int j=0;j<=(col-2); j++){
+                    if (k == j){
+                        a[i][j] = b[i][0];
+                    }
+                    else {
+                        a[i][j] = matrix[i][j];
                     }
                 }
-                //printMatrix(a);
-                //nilai x ke berapa
-                int xNow = k+1;
-                //menghitung nilai x[xNow] = determinan(A[k])/determinan(matrix origin)
-                // Not yet solve
-                double x = Cofactor.determinan(a)/Cofactor.determinan(matrix);
-                //System.out.println(x);
-                System.out.println("Nilai X"+xNow+": "+x);
             }
-
+            //printMatrix(a);
+            //nilai x ke berapa
+            int xNow = k+1;
+            //menghitung nilai x[xNow] = determinan(A[k])/determinan(matrix origin)
+            // Not yet solve
+            double x = Cofactor.determinan(a)/Cofactor.determinan(matrix);
+            //System.out.println(x);
+            result[k] = "Nilai X"+xNow+": "+x;
+            // println("Nilai X"+xNow+": "+x);
         }
-
-
+        return result;
     }
 }
