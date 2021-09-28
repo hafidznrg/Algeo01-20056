a693cc20dd532510283fc946eae1e7d4ab44982d
hafidznrg <hafidznrg.edu@gmail.com>
2021-09-28 14:21:43 +0700

    add file writer regresi

--- src/Regresi.java
+++ src/Regresi.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Regresi extends Menu {
     public static double[] solveRegresi(double[][] matrix, double[] y) {
-        // matrix adalah matriks augmented
         // buat matriks augmented [1|matrix|y] : temp
         int row = matrix.length;
         int col = matrix[0].length;
@@ -23,6 +22,7 @@ public class Regresi extends Menu {
         // displayMat(temp);
         // println();
 
+        // Membuat matrix SPL dengan menggunakan Normal Estimation Equation
         double[][] spl = new double[col + 1][col + 2];
         for (int i = 0; i <= col; i++) {
             for (int j = 0; j <= col + 1; j++) {
@@ -32,21 +32,19 @@ public class Regresi extends Menu {
                 }
             }
         }
-        // println("Matriks SPL");
-        // displayMat(spl);
+        println("\nDengan Normal Estimation Equation, diperoleh matrix SPL sebagai berikut");
+        displayMat(spl);
 
+        // Menyelesaikan matrix SPL dengan metode gauss jordan
         double[][] res = GaussJordan.gaussJordan(spl, col + 1, col + 2);
         // println("Hasil gauss jordan");
         // displayMat(res);
+
+        // Mengambil nilai y saja
         double[] b = new double[col + 1];
         for (int i = 0; i <= col; i++) {
             b[i] = res[i][col + 1];
         }
-
-        // println("Matriks B");
-        // for (int i = 0; i <= col; i++) {
-        // println(b[i]);
-        // }
 
         return b;
     }
@@ -83,34 +81,32 @@ public class Regresi extends Menu {
             n = input.nextInt();
             print("Masukkan banyak persamaan: ");
             m = input.nextInt();
-            print("Masukkan nama file: ");
-            fileName = input.next();
-
-            // Still have a problem
-            raw = FileReadWrite.readFile(fileName, m + 1, n + 2);
+            raw = inputMatrixFile();
             mat = new double[m][n];
             y = new double[m];
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     mat[i][j] = raw[i][j];
                 }
-                y[m] = raw[m][raw.length - 1];
+                y[i] = raw[i][raw[0].length - 1];
             }
 
         }
 
+        // Menyelesaikan regresi linear berganda
         double[] res = solveRegresi(mat, y);
 
-        println("Persamaan regresi yang diperoleh");
-        System.out.printf("y = %.2f", res[0]);
+        // Mengeluarkan persamaan regresi linear berganda
+        println("\nPersamaan regresi linear berganda yang diperoleh");
+        System.out.printf("y = %f", res[0]);
         for (int i = 1; i < res.length; i++) {
             if (res[i] > 0) {
-                System.out.printf(" + %.2f x%d", res[i], i);
+                System.out.printf(" + %f x%d", res[i], i);
             } else {
-                System.out.printf(" %.2f x%d", res[i], i);
+                System.out.printf(" %f x%d", res[i], i);
             }
         }
-        println("\nMenaksir nilai fungsi");
+        println("\n\nMenaksir nilai fungsi");
         System.out.printf("Masukkan %d peubah yang akan ditaksir nilai fungsinya\n", res.length - 1);
         double[] taksir = new double[res.length];
         for (int i = 0; i < res.length - 1; i++) {
@@ -120,8 +116,66 @@ public class Regresi extends Menu {
         for (int i = 0; i < res.length - 1; i++) {
             result += res[i + 1] * taksir[i];
         }
-        System.out.printf("Nilai taksirannya adalah %.2f\n", result);
+        System.out.printf("Nilai taksirannya adalah %f\n\n", result);
 
+        // Memilih menyimpan di file atau tidak
+        displayMenuOutput();
+        int choice = choose(1, 2);
+        if (choice == 1) {
+            try {
+                print("Masukkan path file yang dituju\n> ");
+                String path = input.next();
+                FileWriter myWriter = new FileWriter(path);
+                myWriter.write("Persamaan regresi linear berganda yang diperoleh\ny = ");
+                myWriter.write(Double.toString(res[0]));
+                for (int i = 1; i < res.length; i++) {
+                    if (res[i] > 0) {
+                        myWriter.write(" + ");
+                    }
+                    myWriter.write(Double.toString(res[i]));
+                    myWriter.write("x");
+                    myWriter.write(Integer.toString(i));
+                }
+                // myWriter.write("\n");
+                myWriter.write("\nNilai taksirannya adalah ");
+                myWriter.write(Double.toString(result));
+                myWriter.close();
+                println("Berhasil menuliskan pada " + path);
+            } catch (IOException e) {
+                println("Terjadi error.");
+            }
+        }
+
+    }
+
+    public static double[][] inputMatrixFile() {
+        String fileName;
+        int[] rowsCols;
+        double[][] mat;
+
+        fileName = inputFileName();
+        rowsCols = FileReadWrite.calcRowsCols(fileName);
+        mat = FileReadWrite.readFile(fileName, rowsCols[0], rowsCols[1]);
+
+        return mat;
+    }
+
+    public static String inputFileName() {
+        String fileName;
+        // To check whether the file is exist or not
+        FileReader fr = null;
+
+        println("Masukkan nama file");
+        print("> ");
+        fileName = sc.next();
+        try {
+            fr = new FileReader(fileName);
+        } catch (FileNotFoundException fe) {
+            println("File tidak ditemukan.");
+            fileName = inputFileName();
+        }
+
+        return fileName;
     }
 
     public static void main(String[] args) {
