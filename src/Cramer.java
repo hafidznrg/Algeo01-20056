39e8e3b4f824bc732349cbd41c3f4cc48f7101a9
Fikri Khoiron Fadhila <79701028+fikrikhoironn@users.noreply.github.com>
2021-09-28 20:31:25 +0700

    Update src/Cramer.java
    Co-authored-by: Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>

--- src/Cramer.java
+++ src/Cramer.java
@@ -1,4 +1,4 @@
-public class Cramer {
+public class Cramer extends Utils {
     public static void main(String[] args){
         //double[][] matrix = { { 1.00, 2.00, 3.00 }, { 4.00, 5.00, 6.00 }, { 7.00, 8.00, 9.00 } };
         //double[][] matrix = { { 1.00, 0.00, 0.00 }, { 0.00, 1.00, 0.00 }, { 0.00, 0.00, 1.00 } };
